01ef138786c084c8617905c9008422cdf063fe80
Salixe1214 <alexis.st-amand.1@ulaval.ca>
2022-04-03 11:59:33 -0400

    Adding ue4 project

---
+++ Source/adventureMMO/adventureMMOGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "adventureMMOGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ADVENTUREMMO_API AadventureMMOGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
